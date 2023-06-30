866b2ed31f3eaa5b5a543df6e4a375aa279ab4c9
sebtex1 <36498890+sebtex1@users.noreply.github.com>
2023-06-30 17:32:44 +0200

    Refactor: usage of .env

--- config/database.js
+++ config/database.js
@@ -1,8 +1,9 @@
+require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
 // Création une instance de Sequelize avec les informations de connexion
-const sequelize = new Sequelize('third_account', 'sa', 'Seb12345', {
-        host: 'localhost',
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+        host: process.env.DB_HOST || 'localhost',
         dialect: 'mssql',
         dialectOptions: {
             options: {
--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+require('dotenv').config();
 const express = require('express');
-const app = express();
-const port = 3000;
 const sequelize = require('./config/database');
+
+const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
