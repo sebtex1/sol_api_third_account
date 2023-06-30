449bfeb8defb1dd66d6f1dc2d0e6c8e76e3012f6
sebtex1 <36498890+sebtex1@users.noreply.github.com>
2023-06-30 17:01:58 +0200

    Feat: add creation for thirdAccount (#1)

--- controllers/thirdAccountController.js
+++ controllers/thirdAccountController.js
@@ -2,17 +2,33 @@ const express = require('express');
 const router = express.Router();
 const thirdAccountService = require('../services/thirdAccountService');
 
-const getAllThirdsAccount = async (req, res) => {
+const requestTimeout = (res) => {
     res.setTimeout(5000, () => {
         const errorMessage = 'La requête a expiré après 5 secondes.';
         res.status(500).send(errorMessage);
     });
+};
+
+const getAllThirdAccounts = async (req, res) => {
+    requestTimeout(res);
 
     const thirdsAccount = await thirdAccountService.findAll();
     res.status(200).send(thirdsAccount);
 };
+router.get('/', getAllThirdAccounts);
 
-router.get('/', getAllThirdsAccount);
+const createThirdAccount = async (req, res) => {
+    requestTimeout(res);
+
+    const thirdAccount = req.body;
+    const newThirdAccount = await thirdAccountService.create(thirdAccount);
+    if (newThirdAccount === null) { 
+        res.status(500).send('Erreur lors de la création.');
+        return;
+     }
+    res.status(201).send({ message: 'Création réussie' , id: newThirdAccount.ta_id });
+};
+router.post('/', createThirdAccount);
 
 module.exports = router;
   
