214902166ca106901c736eb941ebd44e52183086
sebtex1 <36498890+sebtex1@users.noreply.github.com>
2023-09-18 16:03:17 +0200

    Update: ajout et focntionnement du docker-compose

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ const { Sequelize } = require('sequelize');
 
 // Création une instance de Sequelize avec les informations de connexion
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-        host: process.env.DB_HOST || 'localhost',
+        host: process.env.DB_SERVER || 'localhost',
         dialect: 'mssql',
         dialectOptions: {
             options: {
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const sequelize = require('./config/database');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8081;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
